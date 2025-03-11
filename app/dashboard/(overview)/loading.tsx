2287d930df91ff0a218580938b10bf942fee25bb
Gabriel Sollero <sollero.gms@gmail.com>
2025-03-11 12:33:14 -0300

    feat: implement streaming in dashboard page

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,6 @@
+// loading.tsx is a reserved file name and will be used automatically by next while page.tsx is loading
+import DashboardSkeleton from '@/app/ui/skeletons';
+
+export default function Loading() {
+  return <DashboardSkeleton />;
+}
